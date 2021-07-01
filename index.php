ebdd380ead2ea1c99f19d388de8f01ee1e7ae3b0
MariusBudreika <mariusbudreika09@gmail.com>
2021-07-01 08:45:54 +0300

    create/delete folder, upload/download file, auth.

---
+++ index.php
@@ -0,0 +1,139 @@
+<?php
+//check
+session_start();
+if (!$_SESSION['logged_in']) {
+    header('Location: ' . 'login.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP web file manager</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    //open/list folders and files + Forms.
+    print('<h1>File manager</h1>');
+    $path = "./" . $_GET['path'];
+    $files = array_diff(scandir($path), array('..', '.'));
+
+    print('<h2>Directory path: ' . $path . '</h2>');
+    print('<table><th>Type</th><th>Name</th><th>Actions</th>');
+    foreach ($files as $file) {
+        print('<tr>');
+        print('<td>' . (is_dir($path . $file) ? "Dir" : "File") . "</td>");
+        print('<td>' . (is_dir($path . $file) ? '<a href="' . (isset($_GET['path'])
+            ? $_SERVER['REQUEST_URI'] . $file . '/'
+            : $_SERVER['REQUEST_URI'] . '?path=' . $file . '/') . '">' . $file . '</a>'
+            : $file) . '</td>');
+            print('<td>'
+            . (is_dir($path . $file)
+                ? ''
+                : '<form style="display: inline-block" action="" method="post">
+                <input type="hidden" name="download" value=' . $file. '>
+                <button id="download" type="submit">Download</button>
+               </form>
+                <form style="display: inline-block" action="" method="post">
+                <input  type="hidden" name="delete" value=' . $file . '>
+                <button id="delete" type="submit">Delete</button>
+                </form>')
+            . "</form></td>");
+        print('</tr>');
+}
+print("</table>");
+
+    // delete
+    if (isset($_POST['delete'])) {
+        unlink('./' . $_GET['path'] . $_POST['delete']);
+        header("Refresh:0.1");
+    }
+
+    // create folders
+    function create()
+    {
+        if (isset($_POST['create'])) {
+            if ($_POST['create'] === "") {
+                echo "Folder name can't be emty string!";
+            }
+            if ($_POST['create'] != "") {
+                $dirCreate = './' . $_GET['path'] . $_POST['create'];
+                if (!is_dir($dirCreate)) {
+                    mkdir($dirCreate);
+                    echo 'Success! Folder created.';
+                    header("Refresh:0.1");
+                }
+                if (is_dir($dirCreate)) {
+                    echo "Folder already exist!";
+                }
+            }
+        }
+    }
+    create();
+
+    // Upload img
+    // print_r($_FILES);
+    if (isset($_FILES['image'])) {
+        $errors = array();
+        $file_name = $_FILES['image']['name'];
+        $file_size = $_FILES['image']['size'];
+        $file_tmp = $_FILES['image']['tmp_name'];
+        $file_type = $_FILES['image']['type'];
+
+        $file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
+        $extensions = array("jpeg", "jpg", "png");
+        if (in_array($file_ext, $extensions) === false) {
+            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+        }
+        if ($file_size > 2097152) {
+            $errors[] = 'File size must be smaller than 2 MB';
+        }
+        if (empty($errors) == true) {
+            move_uploaded_file($file_tmp, $path . $file_name);
+            echo "Succes";
+            header("Refresh:0.1");
+        } else {
+            print_r($errors);
+        }
+    }
+
+    // download
+    if (isset($_POST['download'])) {
+        $file = './' . $_GET["path"] . $_POST['download'];
+        $fileToDownloadEscaped = str_replace("&nbsp;", " ", htmlentities($file, null, 'utf-8'));
+        ob_clean();
+        ob_start();
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: attachment; filename=' . basename($fileToDownloadEscaped));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($fileToDownloadEscaped));
+        ob_end_flush();
+        readfile($fileToDownloadEscaped);
+        exit;
+    }
+    ?>
+
+    <form action="" method="POST" enctype="multipart/form-data">
+        <input type="file" name="image" />
+        <input type="submit" value="Upload" />
+    </form>
+
+    <form action="" method="POST">
+        <input type="text" id="create" name="create" placeholder="write folder name here">
+        <input type="submit" value="Create">
+    </form>
+
+    Click here to <a href="login.php?action=logout"> logout.
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h2>Enter Username and Password</h2>
+</body>
+<?php
+
+session_start();
+// logout logic
+if (isset($_GET['action']) and $_GET['action'] == 'logout') {
+    unset($_SESSION['username']);
+    unset($_SESSION['password']);
+    unset($_SESSION['logged_in']);
+    print('Logged out!');
+}
+// login logic
+$msg = '';
+if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+    if ($_POST['username'] == 'Marius' && $_POST['password'] == '1234') {
+        $_SESSION['logged_in'] = true;
+        $_SESSION['timeout'] = time();
+        $_SESSION['username'] = $_POST['username'];
+        header('Location: ' . 'index.php');
+    } else {
+        $msg = 'Wrong username or password';
+    }
+}
+?>
+
+<div>
+    <form action="./login.php" method="post">
+        <h4><?php echo $msg; ?></h4>
+        <input type="text" name="username" placeholder="username" required autofocus></br>
+        <input type="password" name="password" placeholder="password" required>
+        <button class="btn" type="submit" name="login">Login</button>
+    </form>
+</div>
+
+</html>
